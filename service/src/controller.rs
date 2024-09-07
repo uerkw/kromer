35ddc5d60986a708c730727d49e8cadc606fe677
sovnya <enp7s1@tutanota.com>
2024-09-07 13:40:52 +0200

    Implement method to generate a random address

--- service/src/controller.rs
+++ service/src/controller.rs
@@ -1,7 +1,9 @@
 mod address;
+mod authentication;
 mod name;
 mod transaction;
 
 pub use address::*;
+pub use authentication::*;
 pub use name::*;
 pub use transaction::*;
---
+++ service/src/controller/authentication.rs
@@ -0,0 +1,18 @@
+pub const KROMER_WALLET_ALPHABET: [char; 36] = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+    'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+];
+
+pub struct AuthenticationController;
+
+impl AuthenticationController {
+    /// Create a wallet address
+    ///
+    /// # Example
+    /// ```
+    /// let address: String = AuthenticationController::create_address();
+    /// ```
+    pub fn create_address() -> String {
+        nanoid::nanoid!(10, &KROMER_WALLET_ALPHABET)
+    }
+}
