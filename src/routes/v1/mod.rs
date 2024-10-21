19c768d7838c4f30a70d977819213c0f69267182
sovnya <enp7s1@tutanota.com>
2024-10-21 13:41:41 +0200

    Add /api/v1/version route for Dimmy

--- src/routes/v1/mod.rs
+++ src/routes/v1/mod.rs
@@ -6,6 +6,8 @@ use actix_web::{get, web, HttpResponse};
 
 use crate::errors::KromerError;
 
+static VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct LoginDetail {
     pub password: String,
@@ -16,8 +18,18 @@ async fn index_get() -> Result<HttpResponse, KromerError> {
     Ok(HttpResponse::Ok().body("Hello, world!"))
 }
 
+#[get("/version")]
+async fn version_get() -> Result<HttpResponse, KromerError> {
+    let response = serde_json::json!({
+        "version": VERSION,
+    });
+
+    Ok(HttpResponse::Ok().json(response))
+}
+
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(index_get);
+    cfg.service(version_get);
     cfg.configure(wallet::config);
     cfg.configure(transaction::config);
     cfg.configure(name::config);
