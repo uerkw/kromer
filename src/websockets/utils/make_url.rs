2564cea0b58880e8c7c09e646fcecd082b9d3e44
uerkw <38047633+uerkw@users.noreply.github.com>
2025-01-06 15:40:27 -0500

    wip: handling for URLs and WS vs WSS scheme

--- src/websockets/utils/make_url.rs
+++ src/websockets/utils/make_url.rs
@@ -5,13 +5,16 @@ use surrealdb::Uuid;
 use crate::errors::{websocket::WebSocketError, KromerError};
 
 pub fn make_url(uuid: Uuid) -> Result<String, KromerError> {
-    let schema = "ws";
-    let host =
-        env::var("HOST").map_err(|_| KromerError::WebSocket(WebSocketError::ServerConfigError))?;
-    let port =
-        env::var("PORT").map_err(|_| KromerError::WebSocket(WebSocketError::ServerConfigError))?;
+    let force_insecure = env::var("FORCE_INSECURE").map_err(|_| KromerError::WebSocket(WebSocketError::ServerConfigError))?;
+    let schema = if force_insecure == "true" {"ws"} else {"wss"};
+    // let host =
+        // env::var("HOST").map_err(|_| KromerError::WebSocket(WebSocketError::ServerConfigError))?;
+    // let port =
+        // env::var("PORT").map_err(|_| KromerError::WebSocket(WebSocketError::ServerConfigError))?;
 
-    let server_url = format!("{host}:{port}");
+    // let server_url = format!("{host}:{port}");
+
+    let server_url = env::var("PUBLIC_URL").map_err(|_| KromerError::WebSocket(WebSocketError::ServerConfigError))?;
 
     Ok(format!("{schema}://{server_url}/ws/gateway/{uuid}"))
 }
